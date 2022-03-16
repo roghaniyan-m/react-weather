d6203eaa3d3a7bc49c1096f1de47e99ea977b8ee
m.roghaniyan <m.roghaniyan@CL-126>
2022-03-16 13:18:01 +0330

    changes

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Search from './Search';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Weather Search Engine</h1>
+      <Search />
+    </div>
+  );
+}
+
+export default App;
